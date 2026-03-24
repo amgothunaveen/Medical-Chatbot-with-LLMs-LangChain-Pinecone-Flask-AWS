e6da0228e4109216508e33e1f06d0677b4f0d468
Naveen <amgothunaveen64@gmail.com>
2026-03-24 23:44:59 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="AMGOTHU NAVEEN",
+    author_email="amgothunaveen64@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
